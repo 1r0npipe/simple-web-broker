f75eda055d335e520df3e464a7c583801604b78c
Eugene Makhinya <jesonsev@gmail.com>
2021-04-14 13:58:24 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/1r0npipe/simple-web-broker
+
+go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0
+)
---
+++ internal/app/endpoint/queue.go
@@ -0,0 +1,34 @@
+package endpoint
+
+import (
+	"net/http"
+
+	web_broker "github.com/1r0npipe/simple-web-broker"
+	"github.com/gorilla/mux"
+)
+
+type queueSvc interface {
+	Put(req *web_broker.PutValueReq) error
+	Get(req *web_broker.GetValueReq) (*web_broker.GetValueResp, error)
+}
+
+func RegisterPublicHTTP(queueSvc queueSvc) *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/{queue}", putToQueue(queueSvc)).Methods(http.MethodPut)
+	r.HandleFunc("/{queue}", getFromQueue(queueSvc)).Methods(http.MethodGet)
+
+	return r
+}
+
+func putToQueue(queueSvc queueSvc) http.HandlerFunc {
+	return func(w http.ResponseWriter, request *http.Request) {
+		// TODO: parse req and call queueSvc.Put(...)
+	}
+}
+
+func getFromQueue(queueSvc queueSvc) http.HandlerFunc {
+	return func(w http.ResponseWriter, request *http.Request) {
+		// TODO: parse req and call queueSvc.Get(...)
+	}
+}
